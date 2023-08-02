855fec94e08bfe6a9bd7a9ff3c893e6d52639cef
Preston Lund <prestonlund@gmail.com>
2023-08-02 16:11:26 -0600

    add page schemas

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import clientConfig from "./config/client-config";
+import { Page } from "@/types/Page";
 
 export async function getProjects(): Promise<Project[]> {
 
@@ -30,5 +31,30 @@ export async function getProject(slug: string): Promise<Project> {
             content
         }`,
         { slug }
+    )
+}
+
+export async function getPages(): Promise<Page[]> {
+    return createClient(clientConfig).fetch(
+        groq`*[_type == "page"]{
+            _id,
+            _createdAt,
+            title,
+            "slug": slug.current
+        }`
+    )
+}
+export async function getPage(slug: string): Promise<Page> {
+    return createClient(clientConfig).fetch(
+        groq`*[_type == "page" && slug.current == $slug][0]{
+            _id,
+            _createdAt,
+            title,
+            "slug": slug.current,
+            content
+        }`,
+        {
+            slug
+        }
     )
 }

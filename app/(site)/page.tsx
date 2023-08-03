c98bbb566785680f6c535bf4923897f24120efb8
Preston Lund <prestonlund@gmail.com>
2023-08-03 17:21:56 -0600

    change copy

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,11 +6,11 @@ export default async function Home() {
   const projects = await getProjects();
   return (
     <div>
-      <h1 className="text-7xl font-extrabold">Hello I&apos;m {" "}
+      <h1 className="text-7xl font-extrabold">Hello, I&apos;m {" "}
       <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">Preston</span>!
       </h1>
 
-      <p className="mt-3 text-xl text-gray-600">Hello everyone! Check out my projects!</p>
+      <p className="mt-3 text-xl text-gray-600">Hello everyone! Check out my work!</p>
 
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
 

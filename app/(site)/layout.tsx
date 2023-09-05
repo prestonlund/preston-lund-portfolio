9e01fd2a9eea4acf373437352f4a4a523ab42118
Preston Lund <prestonlund@gmail.com>
2023-09-05 11:14:50 -0600

    change header logo

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
     >
       <body className={`${noto.variable} font-sans max-w-3xl mx-auto py-10`}>
         <header className="flex items-center justify-between">
-          <Link href='/' className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">Preston</Link>
+          <Link href='/' className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">Preston Lund</Link>
 
           <div className="flex items-center gap-5 text-sm text-gray-600">
             {pages.map((page) => (

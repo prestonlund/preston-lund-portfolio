9ad0c5aa43eb1e9feedb7d692cde37e72fcd13ba
Preston Lund <prestonlund@gmail.com>
2024-03-20 11:53:04 -0600

    add 3d cards

---
+++ app/(site)/components/ThreeDCard.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+
+export function ThreeDCard(props: any) {
+    const { imageSrc, projectKey, projectHref, projectName } = props;
+  return (
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto lg:w-[30rem] h-auto rounded-xl p-6 border">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          {projectName}
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Link key={projectKey} className="" href={`/projects/${projectHref}`}>
+            <Image
+              src={imageSrc}
+              height="1000"
+              width="1000"
+              className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+              alt="thumbnail"
+            />
+          </Link>
+        </CardItem>
+        <div className="flex justify-between items-center mt-10">
+          <Link key={projectKey} className="" href={`/projects/${projectHref}`}>
+            <CardItem
+              translateZ={20}
+              as="button"
+              className="hover:bg-purple-700 px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+            >
+              Check it out
+            </CardItem>
+          </Link>
+        </div>
+      </CardBody>
+    </CardContainer>
+  );
+}
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,39 +1,15 @@
 import { getProjects } from "@/sanity/sanity-utils";
-import Image from 'next/image';
-import Link from "next/link";
-import SplineRocket from "./components/SplineRocket";
-import LoadingSpinner from "./components/LoadingSpinner";
 import HeroSection from "./components/HeroSection";
-// import { useState } from "react";
+import { ThreeDCard } from "./components/ThreeDCard";
 
 export default async function Home() {
   const projects = await getProjects();
-
-  // const [isSplineLoaded, setIsSplineLoaded] = useState(false);
-
-  // const handleSplineLoad = () => {
-  //   setIsSplineLoaded(true);
-  // };
   return (
     <div>
       <HeroSection />
-
-      <h2 className="mt-24 font-bold text-gray-700 dark:text-white text-3xl">My Projects</h2>
-
-      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="mt-24 grid grid-cols-1 md:grid-cols-2 gap-8">
         {projects.map((project) => (
-          <Link key={project._id} className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition" href={`/projects/${project.slug}`}>
-            {project.image && (
-              <Image
-              src={project.image}
-              alt={project.name}
-              width={750}
-              height={300}
-              className="object-cover rounded-lg border border-gray-500"
-            />
-            )}
-            <div className="pl-2 mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">{project.name}</div>
-          </Link>
+            <ThreeDCard projectName={project.name} imageSrc={project.image} projectKey={project._id} projectHref={project.slug} />
         ))}
       </div>
     </div>

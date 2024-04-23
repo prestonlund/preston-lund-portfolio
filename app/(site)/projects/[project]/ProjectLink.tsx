0d82e4a3122f19570ebb469de368d19f222b4fe1
Preston Lund <prestonlund@gmail.com>
2024-04-23 16:00:28 -0600

    add load spinner to prev next links

--- app/(site)/projects/[project]/ProjectLink.tsx
+++ app/(site)/projects/[project]/ProjectLink.tsx
@@ -1,20 +1,38 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 import { ChevronLeftIcon, ChevronRightIcon } from "@sanity/icons"; // Import these if needed
 import { Project } from "@/types/Project";
+import { useState } from "react";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 type ProjectLinkProps = {
   project: Project;
   direction: "next" | "prev";
+  isLoading: boolean;
 };
 
-const ProjectLink = ({ project, direction }: ProjectLinkProps) => {
+const ProjectLink = ({ project, direction, isLoading }: ProjectLinkProps) => {
+    const [interanlLoading, setInternalLoading] = useState(false)
+
+    function handleLoading() {
+        setInternalLoading(true);
+        setTimeout(() => {
+            setInternalLoading(false);
+        }, 4500);
+    }
+    
   return (
     <div className={`${direction}-post flex-grow `}>
       <Link
         href={`/projects/${project.slug}`}
+        onClick={() => handleLoading()}
         className={`${direction === "prev" ? "flex-row" : "flex-row-reverse"} group relative flex items-center justify-between overflow-hidden rounded-xl bg-gray-100 no-underline transition-colors hover:bg-gray-200 py-2 `}>
 
+        <div className={`${interanlLoading ? '' : "hidden"} absolute inset-0 flex items-center justify-center bg-gray-300 dark:bg-gray-800 opacity-75 z-10`}>
+            <LoadingSpinner />
+        </div>
         <div className="relative z-10 flex flex-grow justify-center">
           <div className="flex flex-col items-start gap-1">
             <span className="text-sm opacity-50">
--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -64,8 +64,8 @@ export default async function Project({ params }: Props) {
       </div>
 
       <div className=" pt-16 grid gap-4 md:grid-cols-2 lg:gap-64 xl:gap-80">
-        {prevProject ? <ProjectLink project={prevProject} direction="prev" /> : <div> </div>}
-        {nextProject && <ProjectLink project={nextProject} direction="next" />}
+        {prevProject ? <ProjectLink project={prevProject} direction="prev" isLoading={false} /> : <div> </div>}
+        {nextProject && <ProjectLink project={nextProject} direction="next" isLoading={false} />}
       </div>
     </>
   );
